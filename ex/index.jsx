3f02151170db30275e1d7ed9ff8dbddec40e6dcf
José Alexandre <jafidelis@gmail.com>
2018-08-01 22:13:19 -0300

    exercícios 1, 2 e 3

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import Primeiro, { Segundo } from "./componente";
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+ReactDOM.render(
+  <div>
+    <Primeiro />
+    <Segundo />
+  </div>,
+  document.getElementById("app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
         port: 9000,
         contentBase: './public'
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     module: {
         loaders: [{
             test: /\.jsx?$/,
